5e539d4717609dd405b24196c026117fbb9e88ba
Aman Malhotra <49841734+aman003malhotra@users.noreply.github.com>
2023-01-31 13:47:48 +0530

    completed the project

--- script.js
+++ script.js
@@ -4,55 +4,56 @@ let songIndex = 0;
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
-let songItem = Array.from(document.getElementsByClassName("songItem"))
+let songItem = Array.from(document.getElementsByClassName("songItem"));
+let currentSong = document.getElementById("currentSong");
 let songs = [
     {
-        songName: "Salam-e-Ishq",
+        songName: "Despacito",
         filePath: "./songs/1.mp3",
         coverPath:"./covers/1.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Candle in the Wind",
         filePath: "./songs/2.mp3",
         coverPath:"./covers/2.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "In the Summertime",
         filePath: "./songs/3.mp3",
         coverPath:"./covers/3.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Silent Night",
         filePath: "./songs/4.mp3",
         coverPath:"./covers/4.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Rock Around the Clock",
         filePath: "./songs/5.mp3",
         coverPath:"./covers/5.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "I Will Always Love You",
         filePath: "./songs/6.mp3",
         coverPath:"./covers/6.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Something Just Like This",
         filePath: "./songs/7.mp3",
         coverPath:"./covers/7.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Perfect",
         filePath: "./songs/8.mp3",
         coverPath:"./covers/8.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "Shape of You",
         filePath: "./songs/9.mp3",
         coverPath:"./covers/9.jpg"
     },
     {
-        songName: "Salam-e-Ishq",
+        songName: "White Christmas",
         filePath: "./songs/10.mp3",
         coverPath:"./covers/10.jpg"
     }
@@ -102,13 +103,45 @@ makeAllPlays = () =>{
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e) => {
         makeAllPlays();
+        songIndex = parseInt(e.target.id);
         e.target.classList.remove("fa-play-circle");
         e.target.classList.add("fa-pause-circle");
-        audioElement.src = `songs/${parseInt(e.target.id) + 1}.mp3`;
+        currentSong.innerText = songs[songIndex].songName;
+        audioElement.src = `songs/${parseInt(songIndex) + 1}.mp3`;
         audioElement.currentTime = 0;
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
     })
 })
 
+document.getElementById("next").addEventListener('click', () => {
+    if(songIndex >= 9){
+        songIndex = 0;
+    }else{
+        songIndex++;
+    }
+    audioElement.src = `songs/${parseInt(songIndex) + 1}.mp3`;
+    currentSong.innerText = songs[songIndex].songName;
+
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById("previous").addEventListener('click', () => {
+    if(songIndex <= 0){
+        songIndex = 9;
+    }else{
+        songIndex--;
+    }
+    audioElement.src = `songs/${parseInt(songIndex) + 1}.mp3`;
+    currentSong.innerText = songs[songIndex].songName;
+
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+})
